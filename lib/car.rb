6852d29785aa69680342793fb28fbfb47c33bcc7
Annie Pulzone <ajpulzone@gmail.com>
2022-09-05 20:17:06 -0700

    Feature: add paint! method, test passed

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,5 @@
 class Car
-  attr_reader :make, :monthly_payment, :loan_length
+  attr_reader :make, :monthly_payment, :loan_length, :color
 
   def initialize(make, monthly_payment, loan_length, color = nil)
     @make = make
@@ -12,11 +12,8 @@ class Car
     @monthly_payment * @loan_length
   end
 
-  # def color
-  #
-  # end
-  #
-  # def paint!(color)
-  # end
+  def paint!(color)
+    @color = color
+  end
 
 end
