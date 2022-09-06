e5fd5fa79a75e8fb93f166af7775d0ccf281b7ad
Annie Pulzone <ajpulzone@gmail.com>
2022-09-05 19:50:47 -0700

    Feature: initialized class, defined attributes

---
+++ lib/car.rb
@@ -0,0 +1,22 @@
+class Car
+  attr_reader :make, :monthly_payment, :loan_length
+
+  def initialize(make, monthly_payment, loan_length, color = nil)
+    @make = make
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+    @color = color
+  end
+
+  def total_cost
+    @monthly_payment * @loan_length
+  end
+
+  # def color
+  #
+  # end
+  #
+  # def paint!(color)
+  # end
+
+end
