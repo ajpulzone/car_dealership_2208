ada0b7787c53bc6bb63971daea0bc8aacc307c64
Annie Pulzone <ajpulzone@gmail.com>
2022-09-05 20:29:15 -0700

    Test: add and pass test for #add_car method

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -29,6 +29,16 @@ RSpec.describe Dealership do
       expect(@dealership.inventory).to eq([])
       @dealership.add_car(@car_1)
       expect(@dealership.inventory).to eq([@car_1])
+      @dealership.add_car(@car_2)
+      expect(@dealership.inventory).to eq([@car_1, @car_2])
+    end
+
+    it "increases inventory_count by 1 with each car added" do
+      expect(@dealership.inventory_count).to eq(0)
+      @dealership.add_car(@car_1)
+      expect(@dealership.inventory_count).to eq(1)
+      @dealership.add_car(@car_2)
+      expect(@dealership.inventory_count).to eq(2)
     end
   end
 end
