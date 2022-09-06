a87f1b71934c8bae76207cdd5225afeb2989fd61
Annie Pulzone <ajpulzone@gmail.com>
2022-09-05 20:51:21 -0700

    Feature: final commit before submission

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -59,4 +59,29 @@ RSpec.describe Dealership do
       expect(@dealership.has_inventory?).to eq(true)
     end
   end
+
+  # describe "#cars_by_make" do
+  #   it "returns an array containing the cars sorted by the make requested" do
+  #     expect(@dealership.cars_by_make).to eq([])
+  #     @dealership.add_car(@car_1)
+  #     @dealership.add_car(@car_2)
+  #     @dealership.add_car(@car_3)
+  #     @dealership.add_car(@car_4)
+  #     expect(@dealership.cars_by_make("Toyota"))
+  #   end
+  # end
+  #This test will be problematic due to issues with make, moving on through iteration 3
+
+  describe "#total_value" do
+    it "returns the total value of cars in the inventory" do
+      expect(@dealership.inventory).to eq([])
+      expect(@dealership.total_value).to eq(0)
+      @dealership.add_car(@car_1)
+      @dealership.add_car(@car_2)
+      @dealership.add_car(@car_3)
+      @dealership.add_car(@car_4)
+      expect(@dealership.total_value).to eq(156000)
+
+    end
+  end
 end
