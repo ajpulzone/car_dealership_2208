d600ed2ac2de105c265b20656bdc1299471bfbd7
Annie Pulzone <ajpulzone@gmail.com>
2022-09-05 20:19:26 -0700

    Test: add test fpr add_car method, passed

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,34 @@
+require "rspec"
+require "./lib/car"
+require "./lib/dealership"
+
+RSpec.describe Dealership do
+
+  before(:each) do
+    @dealership = Dealership.new("Acme Auto", "123 Main Street")
+    @car_1 = Car.new("Ford Mustang", 1500, 36)
+    @car_2 = Car.new("Toyota Prius", 1000, 48)
+  end
+
+  describe "#initialize" do
+    it "exists" do
+      expect(@dealership).to be_an_instance_of(Dealership)
+    end
+
+    it "inventory starts as an empty array" do
+      expect(@dealership.inventory).to eq([])
+    end
+
+    it "inventory_count starts at 0" do
+      expect(@dealership.inventory_count).to eq(0)
+    end
+  end
+
+  describe "#add_car" do
+    it "adds a car to the inventory array" do
+      expect(@dealership.inventory).to eq([])
+      @dealership.add_car(@car_1)
+      expect(@dealership.inventory).to eq([@car_1])
+    end
+  end
+end
